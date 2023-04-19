18a8a1e882b2e0f8764452aabc7665a7250cf748
Juan Ignacio Garzón <9467722+jigarzon@users.noreply.github.com>
2023-04-19 16:06:43 -0300

    export EntityLocalStorage

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,9 @@ import { EntityFetchCallback } from "./fetcher/AbstractEntityFetcher";
 import { EntitySyncCallback } from "./core/EntitySyncCallback";
 import { DBImplementation } from "./storage/DBImplementation";
 import { buildSynchronizer } from './config/Builder'
+import { EntityLocalStorage } from "./storage/EntityLocalStorage";
 
 export {
-  Synchronizer, EntityDef, EntitySyncCallback, SynchronizerConfig, buildSynchronizer,
+  Synchronizer, EntityDef, EntitySyncCallback, SynchronizerConfig, buildSynchronizer, EntityLocalStorage,
   DBImplementation, EntityFetchCallback, HTTPResponseProcessor, HTTPRequest
 };
