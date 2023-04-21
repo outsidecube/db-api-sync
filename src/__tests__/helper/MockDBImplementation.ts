7165243e4d96ce690e581e1b942f063cbd56b962
Juan Ignacio Garzón <9467722+jigarzon@users.noreply.github.com>
2023-04-21 10:01:43 -0300

    fix tests

--- src/__tests__/helper/MockDBImplementation.ts
+++
@@ -1,8 +0,0 @@
-import { DBImplementation } from "../../storage/DBImplementation";
-
-export class MockDBImplementation implements DBImplementation {
-  async executeSQL(query: string, params: unknown[]): Promise<any> {
-    console.log("EXEC", query,params)
-  }
-
-}
